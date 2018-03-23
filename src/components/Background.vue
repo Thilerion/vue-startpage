1a4fd9f4d7e354e129b7f6617d4304ed3e4630fa
Michael van Meerwijk <michaelvanm@gmail.com>
2018-03-23 22:14:41 +0100

    background load transition

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -1,15 +1,33 @@
 <template>
-	<div class="background" :style="{backgroundImage: `url(${backgroundUrl})`}">
+	<transition name="show-bg">
+	<div v-show="loaded" class="background" :style="{backgroundImage: `url(${backgroundUrl})`}">
 		
 	</div>
+	</transition>
 </template>
 
 <script>
 export default {
+	data() {
+		return {
+			loaded: false
+		}
+	},
 	computed: {
 		backgroundUrl() {
 			return this.$store.getters.backgroundUrl;
 		}
+	},
+	created() {
+		console.log("Loading bg...");
+		console.time("bg");
+		let bgImg = new Image();
+		bgImg.onload = () => {
+			console.log("Bg loaded");
+			console.timeEnd("bg");
+			this.loaded = true;
+		}
+		bgImg.src = this.backgroundUrl;
 	}
 }
 </script>
@@ -35,5 +53,13 @@ export default {
 	height: 100%;
 	width: 100%;
 	background: radial-gradient(ellipse at center, rgba(0, 0, 0, 0) 60%,rgba(0,0,0,0.3) 95%);
+}
+
+.show-bg-enter-active, .show-bg-leave-active {
+	transition: opacity .5s ease;
+}
+
+.show-bg-enter, .show-bg-leave-to {
+	opacity: 0;
 }
 </style>
